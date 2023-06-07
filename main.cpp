cf6acd131e9216565975a76474bde0eb7df65ed9
Michael Zita <mzita2@student.rccd.edu>
2023-06-07 02:26:47 -0700

    Added menu for functionality

--- main.cpp
+++ main.cpp
@@ -10,10 +10,54 @@ string decrypt(string, string);
 int main(){
 
     string key, plaintext, encryption, decryption;
+    int menu;
 
-    key = make_key(key, plaintext);
-    encryption = encrypt(key, plaintext);
-    decryption = decrypt(key, encryption);
+    do{
+        cout << "Welcome to Michael Zita's Vigenere Cipher Program!" << endl;
+        cout << "Please choose an option to continue. " << endl;
+        cout << "0. Exit " << endl;
+        cout << "1. Encrypt a word " << endl;
+        cout << "2. Decrypt a word " << endl;
+        cin >> menu;
+
+        switch(menu){
+            case 0:
+                cout << "Thank you for using the program, have a great day! " << endl;
+                break;
+            case 1:{
+                cout << "Input the plaintext to be encrypted : ";
+                cin >> plaintext;
+
+                cout << "\nInput the keyword to be used: ";
+                cin >> key;
+
+                key = make_key(key, plaintext);
+                encryption = encrypt(key, plaintext);
+
+                cout << "\nEncrypted ciphertext: " << encryption << endl;
+                cout << endl;
+            }
+            break;
+            case 2:{
+                cout << "Input the ciphertext to be decrypted : ";
+                cin >> plaintext;
+
+                cout << "\nInput the keyword used for encryption: ";
+                cin >> key;
+
+                key = make_key(key, plaintext);
+                decryption = decrypt(key, plaintext);
+
+                cout << "\nDecrypted ciphertext: " << decryption << endl;
+                cout << endl;
+            }
+            break;
+            default:
+                cout << "Not an option!" << endl;
+        }
+
+    }while(menu < 3 && menu > 0);
+
 
     return 0;
 
@@ -28,8 +72,6 @@ string make_key(string key, string plaintext){
         new_key = new_key + key[i%key.length()];
 
     }
-
-    cout << new_key << endl;
 
     return new_key;
 }
